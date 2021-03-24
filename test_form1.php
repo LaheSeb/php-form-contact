93813b0341c23f993621fa0e173726083b830588
Seb14 <s.laheyne@laposte.net>
2021-03-24 20:38:51 +0100

    modif

--- test_form1.php
+++ test_form1.php
@@ -5,6 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+          <style>
+              .error {
+                color : red;
+              }
+          </style>
+
 </head>
 <body align=center>
 <?php
@@ -21,48 +27,61 @@
     
     <?php
    
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // collect value of input field
-        if (empty($_POST["name"])){
-            $nameErr = "Name is reqiored";
-        } else {
-            $name = test_input ($_POST)["name"]);
-            if (!preg_match('/^[a-zA-Z \p{L}]+$/ui', $name)) {
-
-                $nameErr = "Only letters and white space allowed";
-        }
-        if (empty($_POST["email"])){
-            $emailErr = "Name is reqiored";
-        } else {
-            $email = test_input ($_POST)["email"]);
-            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                $emailErr = "Invalid email format";
-        }
-        if (!empty($_POST["website"])){
-            $gender = test_input ($_POST)["name"]);
-        }
-        
-        if (!empty($_POST["name"])){
-            $website = "";
-        }else {
-            if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
-                $websiteErr = "Invalid URL";
+   if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["name"])) {
+      $nameErr = "Name is required";
+    } else {
+      $name = test_input($_POST["name"]);
+      // check if name only contains letters and whitespace
+      if (!preg_match('/^[a-zA-Z \p{L}]+$/ui', $name)) {
+        $nameErr = "Only letters and white space allowed";
+      }
     }
+    if (empty($_POST["email"])) {
+        $emailErr = "Email is required";
+      } else {
+        $email = test_input($_POST["email"]);
+        // check if e-mail address is well-formed
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+          $emailErr = "Invalid email format";
         }
-
-
-
-
-
-        
-}
-        
-        echo 'name = '.$_POST['fname'] . '<br/>';
-        echo 'email = '.$_POST['email'] . '<br/>';
-        echo 'website = '.$_POST['website'] . '<br/>';
-        echo 'comment = '.$_POST['comment'] . '<br/>';
-        echo 'gender = '.$_POST['gender'] . '<br/>';
+      }
+      if (empty($_POST["website"])) {
+        $website = "";
+      } else {
+        $website = test_input($_POST["website"]);
+        // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
+        if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)
+          [-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
+          $websiteErr = "Invalid URL";
+        }
+      }
+      if (empty($_POST["comment"])) {
+        $comment = "";
+      } else {
+        $comment = test_input($_POST["comment"]);
+      }
+    
+      if (empty($_POST["gender"])) {
+        $genderErr = "Gender is required";
+      } else {
+        $gender = test_input($_POST["gender"]);
+      }
     }
+
+
+
+
+
+
+
+
+
+        
+
+        
+        
+    
     ?>
 <form method="POST" action ="<?php echo htmlspecialchars ($_SERVER['PHP_SELF']);?>">
        Name:<input type="text" name="name" value="<?php echo $name;?>"><br/>
